072d13d6b44a455b7d098cced0b7171554b5d626
sergioecnetto <sergioecnetto@gmail.com>
2021-05-14 12:02:23 -0300

    Developed Person Creation Operation

---
+++ src/main/java/one/digitalinnovation/personapi/enums/PhoneType.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.personapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+    HOME("Home"),
+    MOBILE("MObile"),
+    COMMERCIAL("Commercial");
+
+    private final String description;
+}
